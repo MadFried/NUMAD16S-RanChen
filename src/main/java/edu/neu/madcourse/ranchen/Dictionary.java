71a19ec01be77b0b69399c3c5c2c5a25f4d98e05
Ran Chen <FredChen@Rans-MacBook-Pro.local>
2016-01-30 18:32:06 -0500

    Dictionary part of UI

---
+++ src/main/java/edu/neu/madcourse/ranchen/Dictionary.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.ranchen;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Dictionary extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+
+}
