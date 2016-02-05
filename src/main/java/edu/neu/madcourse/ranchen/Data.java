ef44ef2626237bf299a16cefcd5687efc76ab2de
Ran Chen <FredChen@Rans-MacBook-Pro.local>
2016-02-05 14:28:55 -0500

    Create a global class to get the data to make it faster

---
+++ src/main/java/edu/neu/madcourse/ranchen/Data.java
@@ -0,0 +1,48 @@
+package edu.neu.madcourse.ranchen;
+
+import android.app.Application;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by FredChen on 2/5/16.
+ */
+public class Data extends Application{
+
+    ArrayList<String> data;
+
+    public Data(){
+    }
+
+    public ArrayList<String> getData() {
+        ArrayList<String> words = null;
+        try {
+            words = ReadFile(getResources().openRawResource(R.raw.wordlist));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+
+    public void setData(ArrayList<String> d) {
+        this.data = d;
+    }
+
+    public ArrayList<String> ReadFile(InputStream inputStream) throws IOException {
+        //InputStream inputStream = getResources().openRawResource(R.raw.wordlist);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String receiveString = null;
+        ArrayList<String> words = new ArrayList<>();
+        while ((receiveString = bufferedReader.readLine()) != null) {
+            words.add(receiveString);
+        }
+        inputStream.close();
+        return words;
+    }
+
+}
--- src/main/java/edu/neu/madcourse/ranchen/Dictionary.java
+++ src/main/java/edu/neu/madcourse/ranchen/Dictionary.java
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.ranchen;
 //
+import java.util.Collections;
 //import android.app.ListActivity;
 //import android.app.LoaderManager;
 //import android.content.Loader;
@@ -7,9 +8,14 @@ package edu.neu.madcourse.ranchen;
 //import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 //import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 //import android.widget.ListView;
@@ -73,6 +79,9 @@ public class Dictionary extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
 
+        Data d = (Data)getApplication();
+        final ArrayList<String> words = d.getData();
+
         View returnButton = this.findViewById(R.id.return_button);
         final Context context = this;
             returnButton.setOnClickListener(new View.OnClickListener() {
@@ -84,20 +93,41 @@ public class Dictionary extends Activity {
             });
 
         View clearButton = this.findViewById(R.id.clear_button);
+        final TextView textView = (TextView) findViewById(R.id.wordshow);
+        final EditText wordLookUp = (EditText) Dictionary.this.findViewById(R.id.textView);
 
-        EditText wordlookup = (EditText) Dictionary.this.findViewById(R.id.textView);
-        String search = wordlookup.getText().toString();
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordLookUp.setText("");
+                textView.setText("");
+            }
+        });
 
-        TextView textView = (TextView) findViewById(R.id.wordshow);
+        wordLookUp.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                try {
-                    String words = read(getResources().openRawResource(R.raw.wordlist));
-                    if (words.equals(search)) {
-                        textView.setText(search);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() >= 3) {
+                    String search = wordLookUp.getText().toString();
+                        if (findWord(words, search)) {
+                            textView.append("\n" + search);
+                            ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+                            toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+                        }
                 }
+            }
+        });
+
 
 
        /* try {
@@ -113,31 +143,21 @@ public class Dictionary extends Activity {
         }*/
     }
 
-    /*public  String[] ReadFile() throws IOException{
-
-            InputStream inputStream = getResources().openRawResource(R.raw.wordlist);
+   /* public ArrayList<String> ReadFile(InputStream inputStream) throws IOException{
+            //InputStream inputStream = getResources().openRawResource(R.raw.wordlist);
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             String receiveString = null;
             ArrayList<String> words = new ArrayList<>();
-
             while ((receiveString = bufferedReader.readLine()) != null) {
                 words.add(receiveString);
             }
             inputStream.close();
-            return words.toArray(new String[words.size()]);
+            return words;
     }*/
-    public boolean findWord(String[] words,String args) {
-        int result = java.util.Arrays.binarySearch(words, args);
-        if (result >= 0){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 
-    public String read(InputStream is) throws Exception {
+
+ /*   public String read(InputStream is) throws Exception {
 //        InputStream is = getResources().openRawResource(R.raw.wordlist);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
@@ -147,15 +167,95 @@ public class Dictionary extends Activity {
         }
         byte[] data = outputStream.toByteArray();
         return  new String(data);
-    }
+    }*/
 
-  /*  public static boolean isHave(String[] strs, String s) {
-        for(int i = 0; i < strs.length; i++) {
-            if(strs[i].indexOf(s) != -1) {
+   /* public char[] read(InputStream is) throws Exception {
+//        InputStream is = getResources().openRawResource(R.raw.wordlist);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = is.read(buffer)) != -1) {
+            outputStream.write(buffer,0, len);
+        }
+        byte[] data = outputStream.toByteArray();
+        String words = data.toString();
+        char[] word = words.toCharArray();
+        return  word;
+    }*/
+
+    /*    public boolean findWord(String[] words,String args) {
+            int result = java.util.Arrays.binarySearch(words, args);
+            if (result >= 0){
                 return true;
             }
+            else {
+                return false;
+            }
+        }*/
+
+  /*  public static boolean isHave(String strs, String s) {
+        boolean bo = false;
+        int result = strs.indexOf(s);
+            if(result != -1) {
+                bo = true;
+            }
+            else {
+                bo = false;
+            }
+        return bo;
+    }*/
+
+    public boolean findWord(ArrayList<String> strs, String s) {
+        Collections.sort(strs);
+        int value = binarySearch(strs,s);
+        if (value != -1) {
+            return true;
         }
-        return false;
+        else {
+            return false;
+        }
+    }
+
+    public static int binarySearch(ArrayList<String> integerList, String searchValue) {
+
+        int low = 0;
+        int high = integerList.size() - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high && !integerList.get(mid).equalsIgnoreCase(searchValue)) {
+
+            if (integerList.get(mid).compareTo(searchValue) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+            mid = (low + high) / 2;
+
+            if (low > high) {
+                mid = -1;
+            }
+
+        }
+        return mid;
+    }
+
+
+
+
+    /*private void clearForm(ViewGroup group)
+    {
+        for (int i = 0, count = group.getChildCount(); i < count; ++i) {
+            View view = group.getChildAt(i);
+            if (view instanceof EditText || view instanceof TextView) {
+                ((EditText)view).setText("");
+                ((TextView) view).setText("");
+            }
+
+
+            if(view instanceof ViewGroup && (((ViewGroup)view).getChildCount() > 0))
+                clearForm((ViewGroup)view);
+        }
     }*/
 
  /*   public void showWord() {
